e146c73858054337224a06824c4b8ab4793c7565
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-08 22:16:03 +0900

    Initial Commit

---
+++ kadai7-UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai7-UIkit
+//
+//  Created by 渡邊魁優 on 2023/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
